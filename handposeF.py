c6d2968cb87615a2c90a10002b7450b6a218a349
Taylor Tam <taylor@taylortam.com>
2024-03-29 14:00:59 -0700

    cleaned up

--- handposeF.py
+++ handposeF.py
@@ -8,10 +8,9 @@ from ultralytics.utils.plotting import Annotator
 import json
 from google.protobuf.json_format import MessageToDict
 
-
+#load some stuff!
 weights_loc = "weights/best.pt"
 yolo_model = YOLO(weights_loc)
-
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 
@@ -21,12 +20,13 @@ mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
 desired_aspect_ratio = 1 
 standard_size = (350, 350)
-handedness = ""
 gestures = ["speed_inc", "speed_dec", "to_right", "to_left"]
 
+#load base gestures
 with open("base_gestures.json", "r") as infile:
     data = json.load(infile)
 
+#preprocessing for gesture matching
 def preprocess_image(img):
     hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     lower_pink = np.array([140, 100, 100])
@@ -35,6 +35,7 @@ def preprocess_image(img):
     result = cv2.bitwise_and(img, img, mask=mask)
     return result
 
+#match gestures w/ orb + bf 
 def match_gestures(handedness, img2, threshold=110):
     print(handedness)
     img2_processed = preprocess_image(img2)
@@ -67,90 +68,101 @@ def match_gestures(handedness, img2, threshold=110):
             print("One or both sets of descriptors are missing or empty.")
     return ""
 
-cap = cv2.VideoCapture(1) 
-
-with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5) as hands:
-    while cap.isOpened():
-        ret, frame = cap.read()
-        if not ret:
-            break  
-
-        #image loading
-        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        image = cv2.flip(image, 1)
-        results = hands.process(image)
-        canvas_for_yolo = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        h, w, c = frame.shape
-
-        #for drawing
-        canvas = np.zeros_like(image)
-
-        #draw results on black canvas
-        if results.multi_hand_landmarks:
-            for handLMs in results.multi_hand_landmarks:
-                mp_drawing.draw_landmarks(
-                    canvas, handLMs, mp_hands.HAND_CONNECTIONS,
-                    mp_drawing.DrawingSpec(color=(121, 22, 76), thickness=2, circle_radius=4),
-                    mp_drawing.DrawingSpec(color=(250, 44, 250), thickness=2, circle_radius=2)
-                )
-            handedness = results.multi_handedness[0].classification[0].label
+#process each frame
+def read_frame(frame, hands): 
+    handedness = ""
+    start = ""
     
+    #image loading
+    image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    image = cv2.flip(image, 1)
+    results = hands.process(image)
+    canvas_for_yolo = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    h, w, c = frame.shape
 
-        width = canvas_for_yolo.shape[1]
+    #for drawing
+    canvas = np.zeros_like(image)
 
-        #perform YOLO predictions
-        yolo_results = yolo_model.predict(frame)
-        for r in yolo_results:
-            #annotate boxes
-            annotator = Annotator(canvas)
-            boxes = r.boxes
-            for box in boxes:
-                b = box.xyxy[0]
-                mirrored_x_min = width - b[2]
-                mirrored_x_max = width - b[0]
-                mirrored_box = [mirrored_x_min-padding, b[1]-padding, mirrored_x_max+padding, b[3]+padding]
-                c = box.cls
+    #draw results on black canvas
+    if results.multi_hand_landmarks:
+        for handLMs in results.multi_hand_landmarks:
+            mp_drawing.draw_landmarks(
+                canvas, handLMs, mp_hands.HAND_CONNECTIONS,
+                mp_drawing.DrawingSpec(color=(121, 22, 76), thickness=2, circle_radius=4),
+                mp_drawing.DrawingSpec(color=(250, 44, 250), thickness=2, circle_radius=2)
+            )
+        handedness = results.multi_handedness[0].classification[0].label
 
-                #create cropped canvas for saving
-                save_me = canvas[max(int(mirrored_box[1]), 0):min(int(mirrored_box[3]), canvas.shape[0]),
-                               max(int(mirrored_box[0]), 0):min(int(mirrored_box[2]), canvas.shape[1])]
+    width = canvas_for_yolo.shape[1]
 
-                h, w = save_me.shape[:2]
-                current_aspect_ratio = w / h
-        
-                # Calculate padding
-                if current_aspect_ratio < desired_aspect_ratio:
-                    # Pad sides
-                    new_width = int(desired_aspect_ratio * h)
-                    pad_width = (new_width - w) // 2
-                    padded_image = cv2.copyMakeBorder(save_me, 0, 0, pad_width, pad_width, cv2.BORDER_CONSTANT, value=[0, 0, 0])
-                else:
-                    # Pad top and bottom
-                    new_height = int(w / desired_aspect_ratio)
-                    pad_height = (new_height - h) // 2
-                    padded_image = cv2.copyMakeBorder(save_me, pad_height, pad_height, 0, 0, cv2.BORDER_CONSTANT, value=[0, 0, 0])
-        
-                # Resize image to standard size
-                resized_image = cv2.resize(padded_image, standard_size, interpolation=cv2.INTER_AREA)
-                base_img  = cv2.imread("base_gestures/start_base.png")
-                start = match_gestures(handedness, resized_image)
-                
-                if len(start) != 0:
-                    cv2.putText(canvas, start, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+    #perform YOLO predictions
+    yolo_results = yolo_model.predict(frame)
 
-                cv2.putText(canvas, "Hand: " + handedness, (50, 120), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+    for r in yolo_results:
+        #annotate boxes
+        annotator = Annotator(canvas)
+        boxes = r.boxes
+        for box in boxes:
+            b = box.xyxy[0]
+            mirrored_x_min = width - b[2]
+            mirrored_x_max = width - b[0]
+            mirrored_box = [mirrored_x_min-padding, b[1]-padding, mirrored_x_max+padding, b[3]+padding]
+            c = box.cls
 
-                if cv2.waitKey(10) & 0xFF == ord('s'):
-                    img_name = f"cropped_hand_{img_counter}.png"
-                    cv2.imwrite("saved_imgs/" + img_name, resized_image)
-                    print(f"{img_name} saved.")
-                    img_counter += 1
+            #create cropped canvas for saving
+            save_me = canvas[max(int(mirrored_box[1]), 0):min(int(mirrored_box[3]), canvas.shape[0]),
+                            max(int(mirrored_box[0]), 0):min(int(mirrored_box[2]), canvas.shape[1])]
 
-        #display
-        cv2.imshow('Hand Skeleton', canvas)
+            h, w = save_me.shape[:2]
+            current_aspect_ratio = w / h
+    
+            # Calculate padding
+            if current_aspect_ratio < desired_aspect_ratio:
+                # Pad sides
+                new_width = int(desired_aspect_ratio * h)
+                pad_width = (new_width - w) // 2
+                padded_image = cv2.copyMakeBorder(save_me, 0, 0, pad_width, pad_width, cv2.BORDER_CONSTANT, value=[0, 0, 0])
+            else:
+                # Pad top and bottom
+                new_height = int(w / desired_aspect_ratio)
+                pad_height = (new_height - h) // 2
+                padded_image = cv2.copyMakeBorder(save_me, pad_height, pad_height, 0, 0, cv2.BORDER_CONSTANT, value=[0, 0, 0])
+    
+            # Resize image to standard size
+            resized_image = cv2.resize(padded_image, standard_size, interpolation=cv2.INTER_AREA)
+            base_img  = cv2.imread("base_gestures/start_base.png")
+            start = match_gestures(handedness, resized_image)
+            
+            if len(start) != 0:
+                cv2.putText(canvas, start, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
 
-        if cv2.waitKey(10) & 0xFF == ord('q'):
-            break
+            cv2.putText(canvas, "Hand: " + handedness, (50, 120), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
 
-cap.release()
-cv2.destroyAllWindows()
+            if cv2.waitKey(10) & 0xFF == ord('s'):
+                img_name = f"cropped_hand_{img_counter}.png"
+                cv2.imwrite("saved_imgs/" + img_name, resized_image)
+                print(f"{img_name} saved.")
+                img_counter += 1        
+    return canvas, start
+
+
+def cap_video():
+    cap = cv2.VideoCapture(1) 
+    with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5) as hands:
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if not ret:
+                break  
+
+            canvas, gesture = read_frame(frame, hands)
+
+            #display
+            cv2.imshow('Hand Skeleton', canvas)
+            if cv2.waitKey(10) & 0xFF == ord('q'):
+                break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+cap_video()
