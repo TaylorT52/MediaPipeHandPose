938b45431a79953cf74170c8220fec15bf276161
Taylor Tam <taylor@taylortam.com>
2024-03-30 13:46:27 -0700

    finished file stuff

--- send_it2.py
+++ send_it2.py
@@ -6,6 +6,40 @@ from nxbt import Buttons
 from nxbt import Sticks
 import time
 
+##### START SEQUENCE #####
+#starts from change grip/order --> Mario Kart
+MACRO = """
+LOOP 12
+    B 0.1s
+    0.1s
+1s
+DPAD_UP 0.25s
+L_STICK@-100+000 0.2s
+A 0.1s
+0.2s
+DPAD_UP 0.25s
+0.1s
+A 0.1s
+"""
+
+##### CONNECT & START #####
+#connects and runs a start sequence
+def connect_controller():
+    print("Connecting...")
+    nx = nxbt.Nxbt()
+    controller_index = nx.create_controller(nxbt.PRO_CONTROLLER)
+    nx.wait_for_connection(controller_index)
+    print("Initialized")
+    #must be blocking 
+    macro_id = nx.macro(controller_index, MACRO, block=True)
+    time.sleep(3)
+    print("Stopping Macro")
+    nx.stop_macro(controller_index, macro_id)
+    print("Stopped Macro")
+    print("Ready to play!")
+
+    return nx, controller_index
+
 ##### CONTROLS #####
 def turn_right(nx, controller_idx):
     print("Turn right")
