addcab9e13f5b5dc1e5746302d8675f2258b660c
Taylor Tam <taylor@taylortam.com>
2024-05-04 12:02:00 -0700

    cap video test

--- handposeF.py
+++ handposeF.py
@@ -8,6 +8,7 @@ import mediapipe as mp
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 from mediapipe.framework.formats import landmark_pb2
+from mediapipe import solutions
 
 from ultralytics import YOLO
 import os
@@ -41,6 +42,10 @@ base_options = python.BaseOptions(model_asset_path='hand_landmarker.task')
 options = vision.HandLandmarkerOptions(base_options=base_options,
                                        num_hands=2)
 detector = vision.HandLandmarker.create_from_options(options)
+MARGIN = 10  # pixels
+FONT_SIZE = 1
+FONT_THICKNESS = 1
+HANDEDNESS_TEXT_COLOR = (88, 205, 54) # vibrant green
 ##########
 
 mp_hands = mp.solutions.hands
@@ -106,8 +111,7 @@ def read_frame(frame, hands):
     #image loading
     image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     image = cv2.flip(image, 1)
-    mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=image)
-    results = detector.detect(mp_image)
+    results = hands.process(image)
     canvas_for_yolo = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     h, w, c = frame.shape
 
@@ -172,6 +176,67 @@ def read_frame(frame, hands):
                 img_counter += 1   
 
     return canvas, start
+
+##### TEST #####
+def draw_landmarks_on_image(rgb_image, detection_result):
+  hand_landmarks_list = detection_result.hand_landmarks
+  handedness_list = detection_result.handedness
+  annotated_image = np.copy(rgb_image)
+
+  # Loop through the detected hands to visualize.
+  for idx in range(len(hand_landmarks_list)):
+    hand_landmarks = hand_landmarks_list[idx]
+    handedness = handedness_list[idx]
+
+    # Draw the hand landmarks.
+    hand_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+    hand_landmarks_proto.landmark.extend([
+      landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in hand_landmarks
+    ])
+    solutions.drawing_utils.draw_landmarks(
+      annotated_image,
+      hand_landmarks_proto,
+      solutions.hands.HAND_CONNECTIONS,
+      solutions.drawing_styles.get_default_hand_landmarks_style(),
+      solutions.drawing_styles.get_default_hand_connections_style())
+
+    # Get the top left corner of the detected hand's bounding box.
+    height, width, _ = annotated_image.shape
+    x_coordinates = [landmark.x for landmark in hand_landmarks]
+    y_coordinates = [landmark.y for landmark in hand_landmarks]
+    text_x = int(min(x_coordinates) * width)
+    text_y = int(min(y_coordinates) * height) - MARGIN
+
+    # Draw handedness (left or right hand) on the image.
+    cv2.putText(annotated_image, f"{handedness[0].category_name}",
+                (text_x, text_y), cv2.FONT_HERSHEY_DUPLEX,
+                FONT_SIZE, HANDEDNESS_TEXT_COLOR, FONT_THICKNESS, cv2.LINE_AA)
+
+  return annotated_image
+
+def read_frame_test(frame):
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame_rgb)
+    detection_result = detector.detect(mp_image)
+    result = draw_landmarks_on_image(frame_rgb, detection_result)
+    return result
+
+def cap_video_test():
+    cap = cv2.VideoCapture('/dev/video0') 
+    cap.set(cv2.CAP_PROP_FPS, 30)
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            print("no ret")
+            break  
+
+        canvas = read_frame_test(frame)
+        #display
+        cv2.imshow('Hand Skeleton', canvas)
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+
+#########
 
 #process the gestures & send to controller
 def process_gesture(gesture, nx, controller_idx):
@@ -243,4 +308,5 @@ def mimic_capture():
 
 
 #run it!
-cap_video()
+# cap_video()
+cap_video_test()
