6fd03dad45b32e0524986e17f34a18f2c394357d
Taylor Tam <taylor@taylortam.com>
2024-05-04 12:46:04 -0700

    added more margin

--- handposeF.py
+++ handposeF.py
@@ -275,10 +275,10 @@ def draw_landmarks_on_image(rgb_image, detection_result):
     height, width, _ = annotated_image.shape
     x_coordinates = [landmark.x for landmark in hand_landmarks]
     y_coordinates = [landmark.y for landmark in hand_landmarks]
-    min_x = int(min(x_coordinates) * width)
+    min_x = int(min(x_coordinates) * width) - MARGIN
     min_y = int(min(y_coordinates) * height) - MARGIN
-    max_x = int(max(x_coordinates) * width)
-    max_y = int(max(y_coordinates) * height) - MARGIN
+    max_x = int(max(x_coordinates) * width) + MARGIN
+    max_y = int(max(y_coordinates) * height) + MARGIN
 
     # Draw handedness (left or right hand) on the image.
     cv2.putText(annotated_image, f"{handedness[0].category_name}",
