1448148a619b5a3c453def12bed586c1b6431060
Taylor Tam <taylor@taylortam.com>
2024-05-05 11:41:50 -0700

    changing turning right

--- send_it2.py
+++ send_it2.py
@@ -65,7 +65,7 @@ def connect_controller():
 ##### CONTROLS #####
 def turn_right(nx, controller_idx):
     print("Turn right")
-    nx.tilt_stick(controller_idx, Sticks.RIGHT_STICK, -100, 0, tilted=0.5)
+    nx.tilt_stick(controller_idx, Sticks.LEFT_STICK, 100, 0, tilted=0.5)
     print("tilt")
     nx.press_buttons(controller_idx, [nxbt.Buttons.A], down=0.2)
     print("done turning right")
