18e885ee9f05faaf7402971b05efad55d85068e9
Taylor Tam <taylor@taylortam.com>
2024-05-08 23:58:16 -0700

    commented out old functions, changed 100-->70

--- send_it2.py
+++ send_it2.py
@@ -75,80 +75,73 @@ def turn_right(nx, controller_idx):
     print("going right")
     input_packet = nx.create_input_packet()
     input_packet["A"] = True
-    input_packet["L_STICK"]["X_VALUE"] = 100
+    input_packet["L_STICK"]["X_VALUE"] = 70
     input_packet["L_STICK"]["Y_VALUE"] = 0
-
     nx.set_controller_input(controller_idx, input_packet)
     time.sleep(0.1)
 
-def turn_right_1(nx, controller_idx):
+# def turn_right_1(nx, controller_idx):
 
-    print("Turn right")
-    nx.press_buttons(controller_idx, [nxbt.Buttons.A], down=0.1)  
-    for tilt in range(10, 51, 10):  
-        nx.tilt_stick(controller_idx, Sticks.LEFT_STICK, tilt, 0, tilted=0.1)
-        nx.press_buttons(controller_idx, [nxbt.Buttons.A], down=0.02) 
-        time.sleep(0.01)
-    nx.tilt_stick(controller_idx, Sticks.LEFT_STICK, 50, 0, tilted=0.1)
-    nx.press_buttons(controller_idx, [nxbt.Buttons.A], down=0.1)  
+#     print("Turn right")
+#     nx.press_buttons(controller_idx, [nxbt.Buttons.A], down=0.1)  
+#     for tilt in range(10, 51, 10):  
+#         nx.tilt_stick(controller_idx, Sticks.LEFT_STICK, tilt, 0, tilted=0.1)
+#         nx.press_buttons(controller_idx, [nxbt.Buttons.A], down=0.02) 
+#         time.sleep(0.01)
+#     nx.tilt_stick(controller_idx, Sticks.LEFT_STICK, 50, 0, tilted=0.1)
+#     nx.press_buttons(controller_idx, [nxbt.Buttons.A], down=0.1)  
 
 def turn_left(nx, controller_idx):
-
     input_packet = nx.create_input_packet()
     input_packet["A"] = True
-    input_packet["L_STICK"]["X_VALUE"] = -100
+    input_packet["L_STICK"]["X_VALUE"] = -70
     input_packet["L_STICK"]["Y_VALUE"] = 0
-
     nx.set_controller_input(controller_idx, input_packet)
     time.sleep(0.1)
 
-def turn_left_1(nx, controller_idx):
-    print("turn left")
-    return
-    nx.press_buttons(controller_idx, [nxbt.Buttons.A], down=0.1) 
-    for tilt in range(-10, -51, -10): 
-        nx.tilt_stick(controller_idx, Sticks.LEFT_STICK, tilt, 0, tilted=0.1)
-        nx.press_buttons(controller_idx, [nxbt.Buttons.A], down=0.02)  
-        time.sleep(0.01) 
-    nx.tilt_stick(controller_idx, Sticks.LEFT_STICK, -50, 0, tilted=0.1)
-    nx.press_buttons(controller_idx, [nxbt.Buttons.A], down=0.1)  
+# def turn_left_1(nx, controller_idx):
+#     print("turn left")
+#     return
+#     nx.press_buttons(controller_idx, [nxbt.Buttons.A], down=0.1) 
+#     for tilt in range(-10, -51, -10): 
+#         nx.tilt_stick(controller_idx, Sticks.LEFT_STICK, tilt, 0, tilted=0.1)
+#         nx.press_buttons(controller_idx, [nxbt.Buttons.A], down=0.02)  
+#         time.sleep(0.01) 
+#     nx.tilt_stick(controller_idx, Sticks.LEFT_STICK, -50, 0, tilted=0.1)
+#     nx.press_buttons(controller_idx, [nxbt.Buttons.A], down=0.1)  
 
 def speed_up(nx, controller_idx, first_press):
     input_packet = nx.create_input_packet()
     input_packet["A"] = True
-
     nx.set_controller_input(controller_idx, input_packet)
     time.sleep(0.1)
 
-def speed_up_1(nx, controller_idx, first_press):
-    print("Speed up!!!!")
-    nx.press_buttons(controller_idx, [nxbt.Buttons.A], down=1.0)
+# def speed_up_1(nx, controller_idx, first_press):
+#     print("Speed up!!!!")
+#     nx.press_buttons(controller_idx, [nxbt.Buttons.A], down=1.0)
 
 def slow_down(nx, controller_idx):
     input_packet = nx.create_input_packet()
     input_packet["A"] = False
-
     nx.set_controller_input(controller_idx, input_packet)
     time.sleep(0.1)
 
-def slow_down_1(nx, controller_idx):
-    nx.press_buttons(controller_idx, [nxbt.Buttons.B], down=0.2)
-    print("Slow down")
+# def slow_down_1(nx, controller_idx):
+#     nx.press_buttons(controller_idx, [nxbt.Buttons.B], down=0.2)
+#     print("Slow down")
 
 def power_up(nx, controller_idx):
     input_packet = nx.create_input_packet()
     input_packet["ZL"] = True
-
+    input_packet["A"] = True
     nx.set_controller_input(controller_idx, input_packet)
     time.sleep(0.1)
-
     print('power up!')
 
 def drift(nx, controller_idx):
     input_packet = nx.create_input_packet()
     input_packet["ZR"] = True
-
+    input_packet["A"] = True
     nx.set_controller_input(controller_idx, input_packet)
     time.sleep(0.1)
-
     print("drift")
